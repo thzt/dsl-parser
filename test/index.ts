80f7307a6de0af3835a3d133d64239debbf77e52
thzt <thzt.xx@gmail.com>
2024-07-05 11:13:23 +0800

    feat: 增加 test case

--- test/index.ts
+++ test/index.ts
@@ -5,10 +5,22 @@ import { Parser } from "../src/index";
 import type { AST } from '../src/index'
 
 describe("test case", () => {
-  const filePath = path.resolve("./test/demo.dsl");
-  const code = fs.readFileSync(filePath, "utf-8");
-
   describe("case 1", () => {
+    const filePath = path.resolve("./test/demo.dsl");
+    const code = fs.readFileSync(filePath, "utf-8");
+
+    it("result 1", () => {
+      debugger;
+      const parser = new Parser(code);
+      const ast: AST = parser.parse();
+      debugger;
+    });
+  });
+
+  describe("case 2", () => {
+    const filePath = path.resolve("./test/demo2.dsl");
+    const code = fs.readFileSync(filePath, "utf-8");
+
     it("result 1", () => {
       debugger;
       const parser = new Parser(code);
